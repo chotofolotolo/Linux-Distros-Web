178e0851835fa179ebd28ff54db963c863b93a55
Ramiro Bazan <ramirobazan35@gmail.com>
2025-08-24 22:25:05 -0300

    .

--- Linux-Distros-Web-main/WEB/js/menu.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const toggle = document.getElementById('menu-toggle');
-  const menu = document.getElementById('menu');
-  toggle.addEventListener('click', function () {
-    menu.classList.toggle('show');
-  });
-});
